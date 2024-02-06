c3118945f324d838193b30775f14e7ddbe4055df
Saurav-Rohilla <156075052+Saurav-Rohilla@users.noreply.github.com>
2024-02-06 10:33:45 +0530

    Add files via upload

---
+++ PHP_task_Database/database.php
@@ -0,0 +1,15 @@
+<?php
+    $serverName = "localhost";
+    $usernName = "root";
+    $password = "";
+    $dbName = "dbphptask";
+
+    $conn = mysqli_connect($serverName, $usernName, $password, $dbName);
+     
+    if(!$conn)
+    {
+        die("Could not connect to server");
+    }
+
+    // mysqli_close($conn);
+?>
